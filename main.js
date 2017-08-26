f60bc2b5b5e1f71d98747b63ea094578f3220981
Ольга Болотова <olbolot@MacBook-Pro-Olga.local>
2017-08-26 20:11:18 +0300

    add basic placemark structure

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ var map;
 
 function init() {
 	map = new ymaps.Map('map', {
-		center: [55.76, 37.64],
+		center: [59.94, 30.32],
         zoom: 12
 	});
+
+	placemark = new ymaps.Placemark([59.97, 30.31], {
+
+	});
+
+	map.geoObjects.add(placemark);
 }
