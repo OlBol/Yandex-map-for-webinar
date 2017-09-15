1239b7e9c8dd652a3c2ba6f450f844c7eb931d84
Ольга Болотова <olbolot@MacBook-Pro-Olga.local>
2017-09-15 22:49:12 +0300

    add markers in forEach

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
 
 ymaps.ready(init);
 
-var map;
+var map,
+    placemarks = [
+    {
+        latitude: 59.97,
+        longitude: 30.3,
+        hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 21',
+            '</div>'
+        ]
+    },
+    {
+        latitude: 59.94,
+        longitude: 30.25,
+        hintContent: '<div class="map__hint">Малый проспект В. О., д. 64к1</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: уМалый проспект В. О., д. 64к1',
+            '</div>'
+        ]
+    },
+    {
+        latitude: 59.93,
+        longitude: 30.34,
+        hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
+            '</div>'
+        ]
+    }];
 
 function init() {
     map = new ymaps.Map('map', {
@@ -9,52 +43,17 @@ function init() {
         zoom: 12
     });
 
-    placemark = new ymaps.Placemark([59.97, 30.30], {
-        hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
-        balloonContentBody: [
-            '<div class="map__balloon">',
-            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
-            'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 21',
-            '</div>'
-        ].join('')
-    }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/map-marker.png',
-        iconImageSize: [46, 57],
-        iconImageOffset: [-23, -57]
-    });
+    placemarks.forEach(function(obj) {
+        placemark = new ymaps.Placemark([obj.latitude, obj.longitude], {
+            hintContent: obj.hintContent,
+            balloonContent: obj.balloonContent.join('')
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map-marker.png',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-23, -57]
+        });
 
-    placemark2 = new ymaps.Placemark([59.94, 30.25], {
-        hintContent: '<div class="map__hint">Малый проспект В. О., д. 64к1</div>',
-        balloonContentBody: [
-            '<div class="map__balloon">',
-            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
-            'Самые вкусные бургеры у нас! Заходите по адресу: уМалый проспект В. О., д. 64к1',
-            '</div>'
-        ].join('')
-    }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/map-marker.png',
-        iconImageSize: [46, 57],
-        iconImageOffset: [-23, -57]
+        map.geoObjects.add(placemark);
     });
-
-    placemark3 = new ymaps.Placemark([59.93, 30.34], {
-        hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
-        balloonContentBody: [
-            '<div class="map__balloon">',
-            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
-            'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
-            '</div>'
-        ].join('')
-    }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/map-marker.png',
-        iconImageSize: [46, 57],
-        iconImageOffset: [-23, -57]
-    });
-
-    map.geoObjects.add(placemark);
-    map.geoObjects.add(placemark2);
-    map.geoObjects.add(placemark3);
 }
