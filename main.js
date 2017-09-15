e2557d1e0359930fa6daa89fdfc188cff05a4a52
Ольга Болотова <olbolot@MacBook-Pro-Olga.local>
2017-09-15 19:41:28 +0300

    add marker

--- main.js
+++ main.js
@@ -10,13 +10,18 @@ function init() {
     });
 
     placemark = new ymaps.Placemark([59.97, 30.31], {
-        hintContent: '<div class="map__hint">ул. Литераторов, д. 21</div>',
+        hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
         balloonContentBody: [
             '<div class="map__balloon">',
             '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
             'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 21',
             '</div>'
         ].join('')
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-marker.png',
+        iconImageSize: [46, 57],
+        iconImageOffset: [-23, -57]
     });
 
     map.geoObjects.add(placemark);
